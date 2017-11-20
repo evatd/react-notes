c7c7d646e77c27d19ea62ff45785f3447ba4b879
Eva <eva.tkavc@gmail.com>
2017-11-20 21:17:33 +0000

    prettier code

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-        // Change the below and you will have a different 'heading',
-        //  Whenever we make change file, we just save it, refresh the browser and we will see the new content pop up, thanks to the transpiler webpack
+      // Change the below and you will have a different 'heading',
+      //  Whenever we make change file, we just save it, refresh the browser and we will see the new content pop up, thanks to the transpiler webpack
       <div>React simple starter</div>
     );
   }
 }
-
